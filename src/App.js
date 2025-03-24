04652f6647b092048ee72f39db78c50f3bc6d2d4
nathan.egodage <nathan.egodage@yahoo.com>
2025-03-24 22:59:13 +1100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import BDMCommissionCalculator from "./BDMCommissionCalculator";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <BDMCommissionCalculator />
+    </div>
+  );
+}
+
+export default App;
